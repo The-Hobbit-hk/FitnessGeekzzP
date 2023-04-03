4df0700f6b5412831d59d77b00c345526eb47cd1
The-Hobbit-hk <harsh.kale672000@gmail.com>
2023-04-03 22:26:34 +0530

    New Updates

--- app/src/main/java/com/example/fitnezzgekzzp/WeightGain.java
+++ app/src/main/java/com/example/fitnezzgekzzp/WeightGain.java
@@ -2,13 +2,27 @@ package com.example.fitnezzgekzzp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WeightGain extends AppCompatActivity {
+
+    Button diet;
+    Button workout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_gain);
+        workout=findViewById(R.id.workout);
+        workout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(WeightGain.this,DaysOp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
